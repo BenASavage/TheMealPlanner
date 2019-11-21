9052d432608535cb1993a40946bb6c0017f53937
BenASavage <44155828+BenASavage@users.noreply.github.com>
2019-11-20 17:47:45 -0700

    Meal List files

--- src/mealplanner/MealPlannerGui.java
+++ src/mealplanner/MealPlannerGui.java
@@ -8,12 +8,16 @@
 package mealplanner;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
 
+
 public class MealPlannerGui {
-    private static MealPlanner planner = deserialize("planner.ser");
+    private static MealPlanner planner = deserialize();
     private static ArrayList<Meal> mealList = planner.getMealList();
 
     public static void main(String[] args) {
@@ -21,7 +25,7 @@ public class MealPlannerGui {
     }
 
     private static void serialize(String filename) {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("planner.ser"))) {
             out.writeObject(planner);
         } catch (IOException e) {
             //Display message that says there was a problem saving the data
@@ -29,13 +33,13 @@ public class MealPlannerGui {
     }
 
     /**
-     * @param filename
+     *
      * @return the previously saved account or a new one if none are found.
      * TODO make this not bare bones. Create new MealPlanner if none are found. User input for the name. File
      * TODO name could be based on the user provided name for the MealPlanner. Opens possibility for multiple users.
      */
-    private static MealPlanner deserialize(String filename) {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+    private static MealPlanner deserialize() {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("planner.ser"))) {
             return (MealPlanner) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
             //Should display a message explaining to the user that no data was found
